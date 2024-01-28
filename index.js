c7ee9a315dab7d1e8d5fa607971b8a2bf828ab0d
Siddharth Jain <siddharthj1405@gmail.com>
2024-01-28 17:29:45 +0530

    Game is now working

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 const cards = document.querySelectorAll(".card");
-const array = [];
-let first = "";
-let n = 0;
+const emojis = [
+  "💎",
+  "🎲",
+  "🎭",
+  "🌟",
+  "🔊",
+  "💎",
+  "💰",
+  "🎀",
+  "🔫",
+  "🌟",
+  "🎀",
+  "🎭",
+  "💰",
+  "🎲",
+  "🔫",
+  "🔊",
+];
+let flipsCount = 0;
+let firstCard = null;
+let secondCard = null;
+let match = 0;
+let clickLock = false;
 
-cards.forEach((card) => {
-  array[card.id] = card.innerHTML;
-  console.log(array);
-  card.innerHTML = "❔";
+const shuffledEmojis = shuffleArray(emojis);
 
+cards.forEach((card, index) => {
+  card.textContent = "❔";
   card.addEventListener("click", () => {
-    card.disabled = true;
-    n++;
-    card.innerHTML = array[card.id];
-    console.log(n);
-    if (n == 1) {
-      first = card;
-    }
-    if (first.innerHTML == card.innerHTML && n == 2) {
-      n = 0;
-      console.log("win");
-    } else if (n == 2) {
-      n = 0;
-      setTimeout(() => {
-        first.innerHTML = "❔";
-        card.innerHTML = "❔";
-      }, 1000);
+    if (clickLock) return;
+    flipsCount++;
+    card.textContent = shuffledEmojis[index];
+    if (flipsCount === 1) {
+      firstCard = card;
+      firstCard.disabled = true;
+    } else if (flipsCount === 2) {
+      secondCard = card;
+      clickLock = true;
+      matchCards(firstCard, secondCard);
+      flipsCount = 0;
     }
   });
 });
+
+function shuffleArray(array) {
+  const shuffledArray = array.slice();
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
+
+function matchCards(card1, card2) {
+  if (card1.textContent === card2.textContent) {
+    match++;
+    if (match === emojis.length / 2) {
+      alert("Congratulations! You've matched all pairs.");
+    }
+    clickLock = false;
+  } else {
+    setTimeout(() => {
+      card1.textContent = "❔";
+      card2.textContent = "❔";
+      card1.disabled = false;
+      clickLock = false;
+    }, 1000);
+  }
+}
